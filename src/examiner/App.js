1b91bf0f2d2e40ef5d1a4133dd03d7fbf31b3bb9
tuyenhn <hntuyen282@gmail.com>
2021-04-04 16:00:17 +0700

    folder restructure

--- src/examiner/App.js
+++ src/examiner/App.js
@@ -1,4 +1,4 @@
-import "./App.css";
+import "./tailwind.css";
 
 function App() {}
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./compiled.css";
 import reportWebVitals from "./reportWebVitals";
 
 function importBuildTarget() {
