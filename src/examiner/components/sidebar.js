cbe244ec95e203cef36687051188bfdeb1435efd
s3818245 <s3818245@rmit.edu.vn>
2021-04-08 15:31:54 +0700

    add nav item list

--- src/examiner/components/sidebar.js
+++ src/examiner/components/sidebar.js
@@ -1,5 +1,23 @@
 import {AiFillCaretRight, AiFillCaretLeft, AiOutlineDashboard, AiOutlineTable, AiFillBook} from "react-icons/ai";
 
 
+	const NavItems = [
+		{
+			title: "Dashboard",
+			path: "/", 
+			icon: <AiOutlineDashboard size={ sidebar ? 22:30}/>
+		}, 
+		{
+			title: "Exam Manager", 
+			path: "/", 
+			icon: <AiOutlineTable size={ sidebar ? 22:30}/>
+		},
+		{
+			title: "Student Manager", 
+			path: "/",
+			icon: <AiFillBook size={ sidebar ? 22:30}/>
+		}
+	];
+
 	const [sidebar, toggleSideBar] = useState(true);
 	const showSideBar = () => toggleSideBar(!sidebar);
