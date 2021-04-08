5eb4538934614803ae3a9cc32b19dd3a5c975bd7
s3818245 <s3818245@rmit.edu.vn>
2021-04-08 15:31:35 +0700

    add useState

--- src/examiner/components/sidebar.js
+++ src/examiner/components/sidebar.js
@@ -1,2 +1,5 @@
 import {AiFillCaretRight, AiFillCaretLeft, AiOutlineDashboard, AiOutlineTable, AiFillBook} from "react-icons/ai";
 
+
+	const [sidebar, toggleSideBar] = useState(true);
+	const showSideBar = () => toggleSideBar(!sidebar);
