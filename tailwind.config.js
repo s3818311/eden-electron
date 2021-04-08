b6c3ba302419731a65e5f132eecc55231278a9a7
s3818245 <s3818245@rmit.edu.vn>
2021-04-08 15:28:31 +0700

    add color for hover state of nav ite,

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        "rmit-red": "#E60028",
-        "rmit-blue": "#000054",
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				"rmit-red": "#E60028",
+				"rmit-blue": "#000054",
+				"nav-hover": "#383895",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
