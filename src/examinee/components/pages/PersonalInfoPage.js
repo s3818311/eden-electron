41f60ef2972f0aa8b960a055eabe3df9a7ea69e1
Luan Vo Thanh <s3822042@rmit.edu.vn>
2021-04-14 20:55:16 +0700

    fix button position error

--- src/examinee/components/pages/PersonalInfoPage.js
+++ src/examinee/components/pages/PersonalInfoPage.js
@@ -1,28 +1,28 @@
 import React from "react";
 
-
 const InfoPageComponent = () => {
   return (
-    <div className="w-full">
-      <div>
-        <div>
+    <div className="w-full h-full flex px-40">
+      <div className="h-10 w-full flex mt-auto mb-20">
+        <div className="w-1/2 flex justify-start">
           <button
             type="submit"
-            className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold mx-auto w-full py-2 px-4 rounded focus:border-none"
+            className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold focus:border-none px-4 py-2 rounded"
           >
             Go back to Login page
           </button>
+        </div>
+        <div className="w-1/2 flex justify-end">
           <button
             type="submit"
-            className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold mx-auto w-full py-2 px-4 rounded focus:border-none"
-          >Next</button>
+            className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold focus:border-none px-4 py-2 rounded"
+          >
+            Next
+          </button>
         </div>
-        <div></div>
       </div>
     </div>
   );
-
 };
-
 
 export default InfoPageComponent;
