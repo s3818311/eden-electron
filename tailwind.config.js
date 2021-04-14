7fdb796a370d3abed87acbfe47fd80b203c56910
Luan Vo Thanh <s3822042@rmit.edu.vn>
2021-04-14 11:08:14 +0700

    add rmit red bold color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {
-      colors: {
-        "rmit-red": "#E60028",
-        "rmit-blue": "#000054",
-      },
-    },
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {
+			colors: {
+				"rmit-red": "#E60028",
+				"rmit-red-bold":"#f0002a",
+				"rmit-blue": "#000054",
+			},
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
 };
