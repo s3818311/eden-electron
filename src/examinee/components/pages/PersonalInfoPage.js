0ec39413d12a9f331651954fc95dbc4b651ee08a
Luan Vo Thanh <s3822042@rmit.edu.vn>
2021-04-14 21:16:40 +0700

    refactor code for more readable

--- src/examinee/components/pages/PersonalInfoPage.js
+++ src/examinee/components/pages/PersonalInfoPage.js
@@ -1,15 +1,22 @@
 import React from "react";
 
-const InfoPageComponent = () => {
+const Text = () => {
   return (
-    <div className="w-full h-full flex px-40">
+    <div className="text-left w-full flex px-20">
+      <p className="text-rmit-blue">Personal Information Confirmation</p>
+    </div>
+  );
+};
+const Button = () => {
+  return (
+    <div className="w-full h-full flex px-20">
       <div className="h-10 w-full flex mt-auto mb-20">
         <div className="w-1/2 flex justify-start">
           <button
             type="submit"
             className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold focus:border-none px-4 py-2 rounded"
           >
-            Go back to Login page
+              Go back to Login page
           </button>
         </div>
         <div className="w-1/2 flex justify-end">
@@ -17,10 +24,19 @@ const InfoPageComponent = () => {
             type="submit"
             className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold focus:border-none px-4 py-2 rounded"
           >
-            Next
+              Next
           </button>
         </div>
       </div>
+    </div>
+  );
+};
+
+const InfoPageComponent = () => {
+  return (
+    <div className="w-full h-screen overflow-hidden">
+      <Text />
+      <Button />
     </div>
   );
 };
