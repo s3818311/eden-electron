15de56f6bddf723e7e67b6f0c00725e05889b7ab
s3818311 <s3818311@rmit.edu.vn>
2021-04-15 21:55:02 +0700

    space size refactor

--- src/examiner/App.js
+++ src/examiner/App.js
@@ -1,17 +1,17 @@
 import React from "react";
 import "tailwindcss/tailwind.css";
-import Sidebar from "./components/Sidebar";
+import Sidebar from "./components/sidebar";
 // import { NavItems } from "./NavItems";
 // import {Route } from "react-router-dom";
 
 
 
 function App() {
-	return (
-		<>  
-			< Sidebar/>
-		</>
-	);
+  return (
+    <>  
+      < Sidebar/>
+    </>
+  );
 }
 
 export default App;
