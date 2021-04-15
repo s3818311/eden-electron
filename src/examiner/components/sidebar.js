dd2bd40ff0155eb2c4d1d365c3e2e995b7f1e485
tuyenhn <hntuyen282@gmail.com>
2021-04-15 23:25:11 +0700

    fix styling and code refactor

--- src/examiner/components/sidebar.js
+++ src/examiner/components/sidebar.js
@@ -1,52 +1,55 @@
-import React, { useState } from "react";	
-import {AiFillCaretRight, AiFillCaretLeft, AiOutlineDashboard, AiOutlineTable, AiFillBook} from "react-icons/ai";
-import {NavLink} from "react-router-dom";
+import React, { useState } from "react";
+import { AiFillCaretRight, AiFillCaretLeft, AiOutlineDashboard, AiOutlineTable, AiFillBook } from "react-icons/ai";
+import { NavLink } from "react-router-dom";
 
 const Sidebar = () => {
-  // use state 
+  // use state
   const [isOpen, toggleSideBar] = useState(true);
   const showSideBar = () => toggleSideBar(!isOpen);
-  const renderBtn = () => isOpen ? <AiFillCaretLeft size={25}/> : <AiFillCaretRight size={25}/>;
-  
+  const renderBtn = () => isOpen ? <AiFillCaretLeft size={25} /> : <AiFillCaretRight size={25}/>;
+
   //list of pages
   const NavItems = [
     {
       title: "Dashboard",
-      path: "/", 
-      icon: <AiOutlineDashboard size={isOpen ? 25 : 30} className="transition-all"/>
-    }, 
-    {
-      title: "Exam Manager", 
-      path: "/exam", 
-      icon: <AiOutlineTable size={isOpen ? 25 : 30} className="transition-all"/>
+      path: "/",
+      icon: <AiOutlineDashboard size={isOpen ? 30 : 35} className="transition-all"/>
     },
     {
-      title: "Student Manager", 
+      title: "Exam Manager",
+      path: "/exam",
+      icon: <AiOutlineTable size={isOpen ? 30 : 35} className="transition-all"/>
+      
+    },
+    {
+      title: "Student Manager",
       path: "/student",
-      icon: <AiFillBook size={isOpen ? 25 : 30} className="transition-all"/>
-    }
+      icon: <AiFillBook size={isOpen ? 30 : 35} className="transition-all"/>,
+    },
   ];
 
   return (
     <>
       <div className={`flex flex-col h-screen bg-rmit-blue transition-all ${isOpen ? "w-3/12" : "w-16"}`}>
-        {NavItems.map( (item, index) => {
+        {
+          NavItems.map((item, index) => {
           // Mapping each item in nav item to li
-          return (
-            <div key={index} className = 'font-serif text-lg text-white pl-4 pt-2 pb-2 hover:bg-nav-hover'> 
-
-				<NavLink exact to={item.path} className ="flex flex-col flex-row" activeClassName="text-red-500 bg-nav-hover" >
-					<div className='inline-block pt-1'>
-						{item.icon} 
-					</div>
-					<div className= {isOpen ? "inline-block pl-2" : "hidden"}>
-						{item.title}
-					</div>
-				</NavLink>   
-
-			</div>
-          );
-        })}
+            return (
+              <NavLink 
+                exact 
+                to={item.path} 
+                className={`py-5 text-white hover:bg-nav-hover ${isOpen ? "px-6" : ""}`} 
+                activeClassName="text-red-500 bg-nav-hover" 
+                key={index}
+              >
+                <div className={`flex flex-row items-center text-xl gap-x-5 ${isOpen ? "" : "justify-center"}`}>
+                  <div className="inline-block">{item.icon}</div>
+                  <div className={isOpen ? "inline-block" : "hidden"}>{item.title}</div>
+                </div>
+              </NavLink>
+            );
+          })
+        }
 
         <div className="w-full h-10 mt-auto cursor-pointer hover:bg-nav-hover" onClick={showSideBar}>
           <div className="flex items-center justify-center h-full text-white">
