470d8974f0f390910bbad2be775fc7c4029071f2
s3818245 <s3818245@rmit.edu.vn>
2021-04-19 18:36:11 +0700

    fix active class issue

--- src/examiner/components/sidebar.js
+++ src/examiner/components/sidebar.js
@@ -9,29 +9,29 @@ const Sidebar = () => {
   const renderBtn = () => isOpen ? <AiFillCaretLeft size={25} /> : <AiFillCaretRight size={25}/>;
 
   //list of pages
-  const NavItems = [
-    {
-      title: "Dashboard",
-      path: "/",
-      icon: <AiOutlineDashboard size={isOpen ? 30 : 35} className="transition-all"/>
-    },
-    {
-      title: "Exam Manager",
-      path: "/exam",
-      icon: <AiOutlineTable size={isOpen ? 30 : 35} className="transition-all"/>
+  // const NavItems = [
+  //   {
+  //     title: "Dashboard",
+  //     path: "/",
+  //     icon: <AiOutlineDashboard size={isOpen ? 30 : 35} className="transition-all"/>
+  //   },
+  //   {
+  //     title: "Exam Manager",
+  //     path: "/exam",
+  //     icon: <AiOutlineTable size={isOpen ? 30 : 35} className="transition-all"/>
 
-    },
-    {
-      title: "Student Manager",
-      path: "/student",
-      icon: <AiFillBook size={isOpen ? 30 : 35} className="transition-all"/>,
-    },
-  ];
+  //   },
+  //   {
+  //     title: "Student Manager",
+  //     path: "/student",
+  //     icon: <AiFillBook size={isOpen ? 30 : 35} className="transition-all"/>,
+  //   },
+  // ];
 
   return (
     <>
       <div className={`flex flex-col h-screen bg-rmit-blue transition-all ${isOpen ? "w-3/12" : "w-16"}`}>
-        {
+        {/* {
           NavItems.map((item, index) => {
           // Mapping each item in nav item to li
             return (
@@ -49,7 +49,39 @@ const Sidebar = () => {
               </NavLink>
             );
           })
-        }
+        } */}
+
+        <NavLink
+          exact
+          to="/"
+          className={`py-5 text-white hover:bg-nav-hover ${isOpen ? "px-6" : ""}`}
+          activeClassName="text-red-500 bg-nav-hover"
+        >
+          <div className={`flex flex-row items-center text-xl gap-x-5 ${isOpen ? "" : "justify-center"}`}>
+            <div className="inline-block"><AiOutlineDashboard size={isOpen ? 30 : 35} className="transition-all"/></div>
+            <div className={isOpen ? "inline-block" : "hidden"}>Dashboard</div>
+          </div>
+        </NavLink>
+        <NavLink
+          to="/exam"
+          className={`py-5 text-white hover:bg-nav-hover ${isOpen ? "px-6" : ""}`}
+          activeClassName="text-red-500 bg-nav-hover"
+        >
+          <div className={`flex flex-row items-center text-xl gap-x-5 ${isOpen ? "" : "justify-center"}`}>
+            <div className="inline-block"><AiOutlineTable size={isOpen ? 30 : 35} className="transition-all"/></div>
+            <div className={isOpen ? "inline-block" : "hidden"}>Exam Manager</div>
+          </div>
+        </NavLink>
+        <NavLink
+          to="/student"
+          className={`py-5 text-white hover:bg-nav-hover ${isOpen ? "px-6" : ""}`}
+          activeClassName="text-red-500 bg-nav-hover"
+        >
+          <div className={`flex flex-row items-center text-xl gap-x-5 ${isOpen ? "" : "justify-center"}`}>
+            <div className="inline-block"><AiFillBook size={isOpen ? 30 : 35} className="transition-all"/></div>
+            <div className={isOpen ? "inline-block" : "hidden"}>Student Manager</div>
+          </div>
+        </NavLink>
 
         <div className="w-full h-10 mt-auto cursor-pointer hover:bg-nav-hover" onClick={showSideBar}>
           <div className="flex items-center justify-center h-full text-white">
