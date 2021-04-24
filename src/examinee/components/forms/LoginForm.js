2ac6afdb00b270e282e14602afc8d815fcffc7f9
s3822042 <s3822042@rmit.edu.vn>
2021-04-24 12:38:47 +0700

    add redirect for login button form

--- src/examinee/components/forms/LoginForm.js
+++ src/examinee/components/forms/LoginForm.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-prototype-builtins */
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 const LoginFormComponent = () => {
   const [credentials, setCredentials] = useState({
@@ -35,12 +36,15 @@ const LoginFormComponent = () => {
   };
 
   return (
-    <form className="w-full h-1/3 grid gap-y-8"
+    <form
+      className="w-full h-1/3 grid gap-y-8"
       onSubmit={handleLoginForm.bind(this)}
-    >	
+    >
       <div className="h-20 px-60 ">
         <div className="bg-rmit-blue w-full h-full flex items-center">
-          <div className=" text-center mx-auto text-2xl text-white">Title of Examination</div>
+          <div className=" text-center mx-auto text-2xl text-white">
+            Title of Examination
+          </div>
         </div>
       </div>
       <div className="w-full px-96">
@@ -48,7 +52,7 @@ const LoginFormComponent = () => {
           className="block text-gray-700 text-sm font-bold mb-2 m-auto"
           htmlFor="username"
         >
-		Student ID
+          Student ID
         </label>
         <input
           id="username"
@@ -57,24 +61,29 @@ const LoginFormComponent = () => {
             (errors.hasOwnProperty("username") ? "border-red-500" : "")
           }
           name="username"
-          type="tel" pattern="[0-9]{7}"
+          type="tel"
+          pattern="[0-9]{7}"
           placeholder="eg.3822042"
           value={credentials.username}
-          onChange={handleInputChange.bind(this)} required
+          onChange={handleInputChange.bind(this)}
+          required
         />
         {errors.hasOwnProperty("username") && (
           <div className="text-red-500 text-xs italic">{errors.username}</div>
         )}
-			
       </div>
-			
+
       <div className=" px-96 w-full">
-        <button type="submit" className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold mx-auto w-full py-2 px-4 rounded focus:border-none">
-          Sign in
-        </button>
+        <Link to="/personal">
+          <button
+            type="submit"
+            className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold mx-auto w-full py-2 px-4 rounded focus:border-none"
+          >
+            Sign in
+          </button>
+        </Link>
       </div>
     </form>
-		
   );
 };
 
