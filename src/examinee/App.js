fa69969d2d0dd1f0f1e77472265300974b13f5a9
s3822042 <s3822042@rmit.edu.vn>
2021-04-24 12:39:23 +0700

    add new route for question page

--- src/examinee/App.js
+++ src/examinee/App.js
@@ -1,18 +1,18 @@
 import "./tailwind.css";
 import React from "react";
-import { BrowserRouter,Switch, Route } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import LoginView from "./views/LoginView";
 import PersonalInfoView from "./views/PersonalInfoView";
-
-
+import QuestionView from "./views/QuestionView";
 
 const App = () => (
   <React.Fragment>
     <BrowserRouter>
       <React.Fragment>
         <Switch>
-          <Route exact path="/login" component={LoginView}/>
+          <Route exact path="/login" component={LoginView} />
           <Route path="/personal" component={PersonalInfoView} />
+          <Route path="/question" component={QuestionView} />
         </Switch>
       </React.Fragment>
     </BrowserRouter>
