9b297e7ecf8aaf46af727f10cecc254caff6d6f1
s3822042 <s3822042@rmit.edu.vn>
2021-04-27 20:50:36 +0700

    fix regex validation

--- src/examinee/components/forms/LoginForm.js
+++ src/examinee/components/forms/LoginForm.js
@@ -1,15 +1,23 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import PropTypes from "prop-types";
+
 import { useForm } from "react-hook-form";
 
-const LoginFormComponent = (props) => {
-  const { register, handleSubmit } = useForm();
+const LoginFormComponent = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
+  const onSubmit = async () => {
+    await fetch("http://localhost:3000/login");
+    // navigate here
+    document.location = "/personal";
+  };
   return (
     <form
       className="w-full h-1/3 grid gap-y-8"
-      onSubmit={handleSubmit(props.loginFunc)}
+      onSubmit={handleSubmit(onSubmit)}
     >
       <div className="h-20 px-60 ">
         <div className="bg-rmit-blue w-full h-full flex items-center">
@@ -26,33 +34,28 @@ const LoginFormComponent = (props) => {
           Student ID
         </label>
         <input
-          id="username"
+          id="studentID"
           className="border mb-2 py-2 px-3 rounded text-gray-700 w-full focus:bg-primary text-2xl"
-          name="username"
+          name="studentID"
           type="tel"
-          pattern="[0-9]{7}"
           placeholder="eg.3822042"
-          required
-          {...register("className")}
+          {...register("studentID", {
+            required: true,
+            pattern: {
+              value: /^[0-9]{7}$/,
+            },
+          })}
         />
-      </div>
-
-      <div className=" px-96 w-full">
-        <Link to="/personal">
-          <button
-            type="submit"
-            className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold mx-auto w-full py-2 px-4 rounded focus:border-none"
-          >
-            Sign in
-          </button>
-        </Link>
+        {errors.studentID && <span role="alert">Incorrect format</span>}
+        <button
+          type="submit"
+          className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold mx-auto w-full py-2 px-4 rounded focus:border-none"
+        >
+          Sign in
+        </button>
       </div>
     </form>
   );
-};
-
-LoginFormComponent.propTypes = {
-  loginFunc: PropTypes.func.isRequired,
 };
 
 export default LoginFormComponent;
