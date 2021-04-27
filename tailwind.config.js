a08d2379e8125ee76ab96fe4b169cfb2fa1eff26
s3822042 <s3822042@rmit.edu.vn>
2021-04-27 17:04:25 +0700

    add text color custom for tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,22 @@
 module.exports = {
-	purge: [],
-	darkMode: false, // or 'media' or 'class'
-	theme: {
-		extend: {
-			colors: {
-				"rmit-red": "#E60028",
-				"rmit-red-bold":"#f0002a",
-				"rmit-blue": "#000054",
-			},
-		},
-	},
-	variants: {
-		extend: {},
-	},
-	plugins: [],
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        "rmit-red": "#E60028",
+        "rmit-red-bold": "#f0002a",
+        "rmit-blue": "#000054",
+      },
+      textColor: {
+        "rmit-blue": "#000054",
+      },
+    },
+  },
+  variants: {
+    extend: {
+      textColor: ["active"],
+    },
+  },
+  plugins: [],
 };
