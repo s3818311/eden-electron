10c18038d38180e0323be7bd57d34f4715be224a
s3822042 <s3822042@rmit.edu.vn>
2021-04-28 13:11:58 +0700

    add red dot icon to the announcement button

--- src/examinee/components/announcement/Announcement.js
+++ src/examinee/components/announcement/Announcement.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Message from "./Message";
 import { FaBars } from "react-icons/fa";
+import { GoPrimitiveDot } from "react-icons/go";
 
 function Announcement() {
   const [showMessage, setShowMessage] = useState(false);
@@ -12,9 +13,10 @@ function Announcement() {
   return (
     <>
       {showMessage && <Message />}
-      <div className="fixed right-0 bottom-0 p-8 ">
+      <div className="fixed right-0 bottom-0 p-8">
+        <GoPrimitiveDot className="text-rmit-red h-6 ml-auto mt-auto absolute right-6 bottom-16" />
         <button
-          className="h-12 w-12 rounded-md  text-white bg-rmit-blue text-3xl leading-10 cursor-pointer "
+          className="h-12 w-12 rounded-md text-white bg-rmit-blue text-3xl leading-10 cursor-pointer "
           onClick={onButtonClickHandler}
         >
           <FaBars className="mx-auto" />
