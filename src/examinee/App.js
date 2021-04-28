f0c9b36b7976cfeb28277c7e42ed9f3ba4d3a3f2
s3822042 <s3822042@rmit.edu.vn>
2021-04-28 12:44:20 +0700

    remove route path for personal

--- src/examinee/App.js
+++ src/examinee/App.js
@@ -3,7 +3,6 @@ import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Landing from "./views/Landing";
 import LoginView from "./views/LoginView";
-import PersonalInfoView from "./views/PersonalInfoView";
 import InstructionView from "./views/InstructionView";
 import QuestionView from "./views/QuestionView";
 
@@ -14,7 +13,6 @@ const App = () => (
         <Switch>
           <Route exact path="/" component={Landing} />
           <Route path="/login" component={LoginView} />
-          <Route path="/personal" component={PersonalInfoView} />
           <Route path="/instruction" component={InstructionView} />
           <Route path="/question" component={QuestionView} />
         </Switch>
