35b3b6eec4cfb926f46ee677d9587af1896bee7e
s3822042 <s3822042@rmit.edu.vn>
2021-04-28 12:44:53 +0700

    add new Instruction component

--- src/examinee/components/pages/InstructionPage.js
+++ src/examinee/components/pages/InstructionPage.js
@@ -1,21 +1,38 @@
 import React from "react";
-import Text from "../text/Title";
 import Instruction from "../text/Instruction";
 import Spinner from "../button/Spinner";
 
+import InfoExaminee from "../text/InfoExaminee";
+
 const InstructionPage = () => {
   return (
-    <div className="w-full flex flex-wrap px-10">
-      <Text title="Instruction" />
-      <di className="w-full flex flex-wrap justify-center items-center">
-        <div className="w-1/2 px-10">
-          <Instruction />
+    <div className="flex h-full">
+      <div className="w-full flex justify-center items-center h-full">
+        <div className="w-2/3 px-10 h-full">
+          <div className="h-1/3 flex items-center">
+            <InfoExaminee />
+          </div>
+          <div className="h-2/3 flex items-center">
+            <Instruction />
+          </div>
         </div>
 
-        <div className="w-1/2 flex justify-center ">
-          <Spinner />
+        <div className="w-1/3 flex justify-center h-full px-10 flex-col">
+          <div className="h-1/3 w-full flex items-center">
+            <div className="text-2xl">Not your info?</div>
+            <button
+              type="submit"
+              className="bg-rmit-red hover:bg-rmit-red-bold text-white font-bold focus:border-none px-4 py-2 rounded mx-auto h-12"
+            >
+              Go Back
+            </button>
+          </div>
+          <div className="h-2/3 w-full flex items-center justify-center flex-col">
+            <Spinner />
+            <div className="pt-5">Please wait for other students to join</div>
+          </div>
         </div>
-      </di>
+      </div>
     </div>
   );
 };
--- src/examinee/views/InstructionView.js
+++ src/examinee/views/InstructionView.js
@@ -2,10 +2,6 @@ import React from "react";
 
 import InstructionPage from "../components/pages/InstructionPage";
 
-const InstructionView = () => (
-  <main className="flex h-full items-center">
-    <InstructionPage />
-  </main>
-);
+const InstructionView = () => <InstructionPage />;
 
 export default InstructionView;
