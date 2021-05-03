85ce6e1633921e81d96851009b12da7a035b0405
tuyenhn <hntuyen282@gmail.com>
2021-05-04 01:27:01 +0700

    redesign

--- src/examiner/components/class_student.js
+++ src/examiner/components/class_student.js
@@ -1,44 +1,49 @@
-import React, {useState} from "react";
+/* eslint-disable no-unused-vars */
+import React, { useState } from "react";
 import useFetch from "react-fetch-hook";
 import NewStudentPopup from "../components/newStudentPopup";
 import Loading from "../components/loading";
+import PropTypes from "prop-types";
 
-const Participants = () => {
+const Students = (props) => {
+  const classList = useFetch("http://localhost:3001/file/classList.json");
 
-  const [currentFile, updateFile] = useState("student1.csv");
+  const getStudentList = (className) => {
+    if (!classList.isLoading) {
+      for (const classObj of classList.data) {
+        if (classObj.name === className){
+          return classObj.participants;
+        }
+      }
+    }
+  };
 
-  const {isLoading, data} = useFetch(`http://localhost:3001/file/${currentFile}`, {
-    formatter: (response) => response.text(),
+  const studentList = useFetch("http://localhost:3001/file/" + getStudentList(props.class), {
+    formatter: (response) => response.text()
   });
 
-
-  const renderParticipants = () => {
-    return isLoading ?
-      (
-        <tr>
-          <td><Loading /></td>
-          <td><Loading /></td>
-          <td><Loading /></td>
-        </tr>) :
-      data.split("\n").map(
-        (line, index) => {
-          const fieldArray = line.split(",");
-          return(
-            <tr key={index} className="border-b-2 border-black border-opacity-20">
-              <td className="px-2 py-2 text-center grid justify-items-center">
-                <img src={fieldArray[0]}
-                  className="items-center object-scale-down h-28"
-                  alt="profilepic"></img>
-              </td>
-              <td className="px-2 py-2 text-center">
-                {fieldArray[1]}
-              </td>
-              <td className="px-2 py-2 text-center">
-                {fieldArray[2]}
-              </td>
-            </tr>
-          );
-        }
+  const renderStudents = () => {
+    return studentList.isLoading
+      ? <tr><td><Loading/></td></tr>
+      : (
+        studentList.data.split("\n").map(
+          (line, index) => {
+            const fieldArray = line.split(",");
+            return(
+              <tr key={index} className="border-b-2 border-black border-opacity-20">
+                <td className="px-2 py-2 text-center grid justify-items-center">
+                  {fieldArray[0]}
+                </td>
+                <td className="px-2 py-2 text-center">
+                  {fieldArray[1]}
+                </td>
+                <td className="px-2 py-2 text-center">
+                  {fieldArray[2]}
+                </td>
+              </tr>
+            );
+          }
+        )
       );
   };
 
@@ -46,38 +51,58 @@ const Participants = () => {
 
   const toggleModal = () => {setModal(!modalIsOpen);};
 
-  const renderModal = () => {
-    if(modalIsOpen){
-      return (<NewStudentPopup currentFile={currentFile} text="Choose new student list" closePopup={toggleModal} updateFile={updateFile}/>);
-    }
+  const selectStudentList = (evt) => {
+    const studentList = evt.currentTarget.id;
+
+    fetch(`http://localhost:3001/update/class/${props.class}/students/${studentList}`, {
+      method: "PATCH",
+      body: studentList,
+      headers: {
+        "Content-Type": "text"
+      },
+    }).then(() => {
+      window.location.reload();
+    });
   };
 
-
-  return (
-    <div className="w-full">
-      <div className="flex justify-end w-full">
-        <div className="inline-block px-5 py-1 text-white cursor-pointer rounded-md bg-rmit-red" onClick={toggleModal}>
-            Update students
-        </div>
-      </div>
-
-      <div className="px-20 py-10">
-        <table className="w-full table-fixed">
+  const renderTable = () => {
+    return (getStudentList(props.class) === null)
+      ? (
+        <>
+          <div className="flex justify-center w-full pt-10">
+            <div className="inline-block px-5 py-1 text-white cursor-pointer rounded-md bg-rmit-red" onClick={toggleModal}>
+              Add students
+            </div>
+          </div>
+          {modalIsOpen && <NewStudentPopup closePopup={toggleModal} selectFunc={selectStudentList}/>}
+        </>
+      )
+      : (
+        <table className="w-full table-auto">
           <thead>
-            <tr className="text-rmit-blue bg-rmit-blue bg-opacity-10">
-              <th>Profile Picture</th>
-              <th>Name</th>
-              <th>Date of birth</th>
+            <tr className="text-rmit-blue">
+              <th className="sticky top-0 py-2 bg-blue-200">Student ID</th>
+              <th className="sticky top-0 py-2 bg-blue-200">Name</th>
+              <th className="sticky top-0 py-2 bg-blue-200">Date of Birth</th>
             </tr>
           </thead>
           <tbody>
-            {renderParticipants()}
+            {renderStudents()}
           </tbody>
         </table>
-      </div>
-      {renderModal()}
-    </div>
-  );
+      );
+  };
+
+  return classList.isLoading
+    ? <Loading />
+    : (
+      renderTable()
+    );
 };
 
-export default Participants;
+Students.propTypes = {
+  class : PropTypes.string.isRequired,
+};
+
+
+export default Students;
--- src/examiner/components/newStudentPopup.js
+++ src/examiner/components/newStudentPopup.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import Loading from "../components/loading";
 // import { useForm } from "react-hook-form";
@@ -7,19 +7,19 @@ import {AiFillFileText} from "react-icons/ai";
 
 
 const NewStudentPopup = (props) => {
-  const [currentFile, setFile] = useState(props.currentFile);
-
-  const getStudentFileList = useFetch("http://localhost:3001/file/studentFiles.json", {
-    formatter: (response) => response.text(),
-  });
+  let studentFiles = useFetch("http://localhost:3001/file/studentFiles.json");
 
   const renderFileList = () => {
-    return getStudentFileList.isLoading
-      ? <Loading></Loading>
-      : JSON.parse(getStudentFileList.data).map((item, index) => {
+    return studentFiles.isLoading
+      ? <Loading className="col-span-3" />
+      : studentFiles.data.map((item, index) => {
         return (
-          <div key={index} className={`flex flex-col items-center cursor-pointer text-rmit-blue ${currentFile===item.filename && "bg-rmit-blue bg-opacity-10"}`}
-            onClick={()=>{setFile(item.filename);}}>
+          <div
+            key={index}
+            className="flex flex-col items-center py-2 cursor-pointer text-rmit-blue hover:bg-rmit-blue hover:bg-opacity-10 transition-all"
+            onClick={props.selectFunc}
+            id={item.filename}
+          >
             <div className="text-5xl">
               <AiFillFileText />
             </div>
@@ -34,19 +34,14 @@ const NewStudentPopup = (props) => {
   return (
     <div className="fixed top-0 left-0 w-full h-screen bg-black bg-opacity-25">
       <div className="relative w-1/3 h-auto m-auto overflow-auto bg-white rounded-lg top-1/4">
-        <div>
-          <div className="p-5 grid gap-y-5 gap-x-7 grid-cols-2">
-            <div className="text-lg text-center col-span-2">{props.text}</div>
-            <div className="grid grid-cols-3 col-span-2">
-              {renderFileList()}
-            </div>
-            <button onClick={props.closePopup} className="inline-block px-5 py-1 text-center text-white bg-red-500 cursor-pointer rounded-md">
-              Close
-            </button>
-            <button className="inline-block px-5 py-1 text-center text-white bg-green-500 cursor-pointer rounded-md" onClick={()=>{props.closePopup(); props.updateFile(currentFile);}}>
-              Update
-            </button>
+        <div className="p-5 grid gap-y-5 gap-x-7">
+          <div className="text-lg text-center">Select a student list</div>
+          <div className={studentFiles.isLoading ? "flex pb-10" : "grid grid-cols-3"}>
+            {renderFileList()}
           </div>
+          <button onClick={props.closePopup} className="inline-block px-5 py-1 text-center text-white bg-red-500 cursor-pointer rounded-md">
+              Close
+          </button>
         </div>
       </div>
     </div>
@@ -54,10 +49,8 @@ const NewStudentPopup = (props) => {
 };
 
 NewStudentPopup.propTypes = {
-  text: PropTypes.string.isRequired,
   closePopup: PropTypes.func.isRequired,
-  currentFile: PropTypes.string.isRequired,
-  updateFile: PropTypes.func.isRequired,
+  selectFunc: PropTypes.func.isRequired,
 };
 
 export default NewStudentPopup;
