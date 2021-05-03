29607a6d5a398a4cc7060df0b5ae628a046ac699
tuyenhn <hntuyen282@gmail.com>
2021-05-04 01:26:32 +0700

    redesign and use form hook

--- src/examiner/components/class_startexam.js
+++ src/examiner/components/class_startexam.js
@@ -1,122 +1,135 @@
-import React, {useState} from "react";
+import React from "react";
 import useFetch from "react-fetch-hook";
 import SelectParticipant from "../components/select_participants";
-import Loading from "../components/loading";
-
+import { useForm } from "react-hook-form";
 
 const StartExam = () => {
-  const [showParticipant, toggleSelection] = useState(false);
+  const { register, watch } = useForm();
+
+  const timeLimit = watch("hasTimeLimit");
+  const autoGrading = watch("autoGrading", false);
+  const showParticipant = watch("participant", "all");
 
   const renderExamList = () => {
-    const {isLoading, data} = useFetch("http://localhost:3001/file/examList.json");
-    return isLoading ? <Loading /> :
-      data.map(
-        (item, index) => {
-          return(
-            <option value={item.fileName} key={index}>{item.title}</option>
-          );
-        }
-      );
+    let {isLoading, data} = useFetch("http://localhost:3001/file/examList.json");
+    return isLoading
+      ? null
+      : data.map((item, index) => {
+        return(
+          <option value={item.fileName} key={index}>{item.title}</option>
+        );
+      });
   };
 
+
   return (
-    <div className="w-full">
-      <div className="flex justify-end w-full">
-        <div className="inline-block px-5 py-1 text-white cursor-pointer rounded-md bg-rmit-red">
-            Start Exam
-        </div>
-      </div>
-
-
-      <div className = "flex justify-center px-20 py-10">
-        <div className = "flex flex-row">
-          <form>
-            <div className="grid grid-rows-2 gap-5">
-
-              <div className="grid grid-cols-2 gap-2">
-                <div className="text-lg text-rmit-blue">
+    <>
+      <form className="flex justify-center pt-10">
+        <div className="w-1/2 grid auto-rows-auto gap-y-5">
+          <div className="flex flex-row items-center">
+            <div className="w-1/2 text-lg text-rmit-blue">
                 Select Exam
-                </div>
-                <select className="inline-block px-2 py-1 border rounded-md" name="exam" id="exam-input">
-                  {renderExamList()}
-                </select>
-              </div>
-
-
-              <div className="grid grid-cols-2 gap-2">
-                <div className="text-lg text-rmit-blue">
-                Select Participants
-                </div>
-                <div>
-                  <select className="inline-block px-2 py-1 border rounded-md" name="participant" id="participant-input" onChange={(event)=>{event.target.value == "select" ? toggleSelection(true) : toggleSelection(false);}}>
-                    <option selected value="default">All students (default)</option>
-                    <option value="select">Select students</option>
-                  </select>
-                </div>
-              </div>
-
-              {showParticipant && <SelectParticipant /> }
-
-              <div>
-                <div className="text-lg text-rmit-blue">
-                  Exam settings
-                </div>
-
-                <div className="grid grid-rows-2 gap-2">
-                  <div className="pl-7">
-                    <input type="checkbox" name="suffle" id=""/>
-                    <div className="inline-block px-5">
-                    Shuffle answers
-                    </div>
-                  </div>
-
-                  <div className="pl-7">
-                    <input type="checkbox" name="time-limit" id=""/>
-                    <div className="inline-block px-5">
-                    Time limit
-                    </div>
-                    <input type="text" className="w-8 border rounded-sm focus:outline-none focus:ring-2 focus:ring-blue-600" name="time"/>
-                    <div className="inline-block px-2">
-                      minutes
-                    </div>
-                  </div>
-                </div>
-
-              </div>
-
-              <div>
-                <div className="text-lg text-rmit-blue">
-                  Grading settings
-                </div>
-
-                <div className="pl-7">
-                  <input type="checkbox" name="auto-grade" id=""/>
-                  <div className="inline-block px-5">
-                    Auto grading (multiple choice questions)
-                  </div>
-                </div>
-
-              </div>
-
-              <div>
-                <div className="text-lg text-rmit-blue">
-                  Display grade
-                </div>
-
-                <div className="pl-7">
-                  <input type="checkbox" name="show-grade" id=""/>
-                  <div className="inline-block px-5">
-                    Auto show grades
-                  </div>
-                </div>
-
-              </div>
-
             </div>
-          </form>
+            <select
+              className="inline-block w-1/2 px-2 py-1 border transition-all rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+              {...register("exam")}
+            >
+              {renderExamList()}
+            </select>
+          </div>
+
+          <div className="flex flex-row items-center">
+            <div className="w-1/2 text-lg text-rmit-blue">
+                Select Participants
+            </div>
+            <select
+              className="inline-block w-1/2 px-2 py-1 border transition-all rounded-md focus:outline-none focus:ring-2 focus:ring-blue-600"
+              {...register("participant")}
+            >
+              <option value="all">All students</option>
+              <option value="custom">Select students</option>
+            </select>
+          </div>
+
+          { (showParticipant === "custom") && <SelectParticipant /> }
+
+          <div className="grid grid-rows-3 gap-y-1">
+            <div className="text-lg text-rmit-blue">
+              Exam settings
+            </div>
+
+            <div className="flex flex-row items-center pl-7">
+              <input type="checkbox" className="w-4 h-5" {...register("shuffleQuestions")} />
+              <div className="px-5">
+                Shuffle questions
+              </div>
+            </div>
+
+            <div className="flex flex-row items-center pl-7">
+              <input type="checkbox" className="w-4 h-5" {...register("shuffleAnswers")} />
+              <div className="px-5">
+                Shuffle answers
+              </div>
+            </div>
+
+            <div className="flex flex-row items-center pl-7">
+              <input type="checkbox" className="w-4 h-5" {...register("hasTimeLimit")} />
+              <div className="inline-block px-5">
+                Time limit
+              </div>
+              { timeLimit && (
+                <>
+                  <input
+                    type="number"
+                    className="px-2 border rounded-sm w-14 focus:outline-none focus:ring-2 focus:ring-blue-600"
+                    {...register("timeLimit")}
+                  />
+                  <div className="inline-block px-2">
+                    minutes
+                  </div>
+                </>
+              )}
+            </div>
+          </div>
+
+          <div className="grid auto-rows-auto gap-y-1">
+            <div className="text-lg text-rmit-blue">
+              Grading settings
+            </div>
+
+            <div className="flex flex-row items-center pl-7">
+              <input type="checkbox" className="w-4 h-5" {...register("autoGrading")} />
+              <div className="inline-block px-5">
+                Auto grading
+              </div>
+            </div>
+          </div>
+
+          { autoGrading && (
+            <div className="grid auto-rows-auto gap-y-1">
+              <div className="text-lg text-rmit-blue">
+              Display grade
+              </div>
+              <div className="flex flex-row items-center pl-7">
+                <input type="checkbox" className="w-4 h-5" {...register("autoShowGrade")} />
+                <div className="inline-block px-5">
+                Auto show grades
+                </div>
+              </div>
+            </div>
+          )}
+
+          <div className="flex justify-center">
+            <button
+              className="px-5 py-1 text-white cursor-pointer rounded-md bg-rmit-red"
+              type="submit"
+            >
+              Start Exam
+            </button>
+          </div>
         </div>
-      </div>
-    </div>
+      </form>
+    </>
   );
 };
 
--- src/examiner/components/select_participants.js
+++ src/examiner/components/select_participants.js
@@ -1,34 +1,32 @@
 import React from "react";
 import useFetch from "react-fetch-hook";
 import Loading from "../components/loading";
-
+import { useForm } from "react-hook-form";
 
 const SelectParticipants = () => {
-
-  const {isLoading, data} = useFetch("http://localhost:3001/file/student1.csv", {
-    formatter: (response) => response.text(),
-  });
+  const { register } = useForm();
 
   const renderStudentList = () => {
-    return isLoading ? <Loading /> :
-      data.split("\n").map(
-        (line, index) => {
-          const fieldArray = line.split(",");
-          return(
-            <div key={index}>
-              <input type="checkbox" checked name="" id=""/>
-              <div className="inline-block px-2">
-                {fieldArray[1]}
-              </div>
-            </div>
-          );
-        }
-      );
+    const { isLoading, data } = useFetch("http://localhost:3001/file/student1.csv", {
+      formatter: (response) => response.text(),
+    });
+
+    return isLoading
+      ? <Loading />
+      : data.split("\n").map((line, index) => {
+        const studentInfo = line.split(",");
+        return (
+          <div key={index} className="flex flex-row items-center">
+            <input type="checkbox" className="w-4 h-5" {...register(studentInfo[1])} />
+            <div className="inline-block px-2 text-lg">{studentInfo[1]}</div>
+          </div>
+        );
+      });
   };
 
   return (
-    <div className="p-2 border border-gray">
-      <div className="overflow-auto h-52">
+    <div className="h-52">
+      <div className="w-2/3 h-full pt-2 pl-5 mx-auto overflow-auto border border-gray rounded-md">
         {renderStudentList()}
       </div>
     </div>
