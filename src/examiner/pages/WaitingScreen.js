5cce1b97f3a38788808ff72c4c334fe139b81de4
tuyenhn <hntuyen282@gmail.com>
2021-05-27 02:23:41 +0700

    link waiting screen to back end

--- src/examiner/pages/WaitingScreen.js
+++ src/examiner/pages/WaitingScreen.js
@@ -1,30 +1,56 @@
-import React, { useState } from "react";
+import React from "react";
+import useFetch from "react-fetch-hook";
+import Loading from "../components/loading";
+
+const getExamId = () => {
+  return new URLSearchParams(window.location.search).get("examId");
+};
+
+const StudentList = (props) => {
+  return props.attendingStudents.data.map((student, idx) => (
+    <div
+      key={idx}
+      className={`m-2 ${student.status === "NONE" ? "animate-pulse" : ""}`}
+    >
+      <div
+        className={`p-2 ${
+          student.status === "NONE"
+            ? "bg-gray-100"
+            : student.status === "WAITING"
+            ? "bg-yellow-200"
+            : ""
+        }`}
+      >
+        <div className="inline-block w-1/2">{student.name}</div>
+        <div className="inline-block w-1/2 text-right">
+          {student.status === "NONE"
+            ? "Not entered"
+            : student.status === "WAITING"
+            ? "Waiting"
+            : ""}
+        </div>
+      </div>
+    </div>
+  ));
+};
 
 const WaitingScreen = () => {
-  // eslint-disable-next-line no-unused-vars
-  const [studentList, setStudentList] = useState([
-    {
-      name: "Student 1",
-    },
-    {
-      name: "Student 2",
-    },
-    {
-      name: "Student 3",
-    },
-    {
-      name: "Student 4",
-    },
-    {
-      name: "Student 5",
-    },
-    {
-      name: "Student 6",
-    },
-    {
-      name: "Student 7",
-    },
-  ]);
+  const attendingStudents = useFetch(
+    "http://localhost:3001/studentTakesExam/" + getExamId()
+  );
+
+  const exam = useFetch("http://localhost:3001/exams/" + getExamId());
+
+  const checkAllJoined = () => {
+    if (attendingStudents.isLoading) return false;
+
+    let allJoined = true;
+    attendingStudents.data.forEach((obj) => {
+      if (obj.status !== "WAITING") allJoined = false;
+    });
+
+    return allJoined;
+  };
 
   return (
     <div className="flex items-center w-screen h-screen">
@@ -32,27 +58,25 @@ const WaitingScreen = () => {
         <div className="h-16 px-32 ">
           <div className="flex items-center w-full h-full rounded-sm bg-rmit-blue">
             <div className="mx-auto text-2xl text-center text-white">
-              Title of Examination
+              {exam.isLoading ? <Loading /> : exam.data.title}
             </div>
           </div>
         </div>
         <div className="h-12 px-32 ">
           <div className="flex items-center w-full h-full rounded-sm bg-wating-banner bg-opacity-70">
-            <div className="mx-auto text-xl text-center text-white">
-              Wating for examinees to join
+            <div className="mx-auto text-xl text-center text-black">
+              {checkAllJoined()
+                ? "All examinees joined"
+                : "Wating for examinees to join ..."}
             </div>
           </div>
         </div>
 
         <div className="px-36">
-          <div className="w-full h-48 p-2 overflow-auto border-2">
-            {studentList.map((item, index) => {
-              return (
-                <div key={index} className="py-2">
-                  {item.name}
-                </div>
-              );
-            })}
+          <div className="w-full h-48 p-4 overflow-auto border-2">
+            {!attendingStudents.isLoading && (
+              <StudentList attendingStudents={attendingStudents} />
+            )}
           </div>
           <div className="flex justify-end w-full py-2">
             <button
