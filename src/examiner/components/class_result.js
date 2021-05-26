9543e1a94359b2b3865df18bfd6ad0d3ecf34db7
tuyenhn <hntuyen282@gmail.com>
2021-05-26 14:06:22 +0700

    fix result page

--- src/examiner/components/class_result.js
+++ src/examiner/components/class_result.js
@@ -1,7 +1,46 @@
+/* eslint-disable no-unused-vars */
 import React, { useState } from "react";
 import useFetch from "react-fetch-hook";
 import PropTypes from "prop-types";
 import Loading from "./loading";
+
+const Exams = (props) => {
+  if (props.exams.data.length != 0) {
+    props.setHasData(true);
+    return props.exams.data.map((exam, index) => (
+      <th key={index} className="sticky top-0 py-2 bg-blue-200">
+        {exam.title}
+      </th>
+    ));
+  }
+};
+
+const ExamMarks = (props) => {
+  return props.exams.data.map((exam, index) => {
+    const mark = props.getMark(props.studentId, exam.id);
+    return (
+      <td key={index} className="px-2 py-1 text-center">
+        {mark === null ? "DNA" : mark}
+      </td>
+    );
+  });
+};
+
+const Results = (props) => {
+  return props.students.data.map((student, index) => (
+    <tr key={index} className="border-b-2 border-black border-opacity-20">
+      <td className="px-2 py-1 text-center grid justify-items-center">
+        {student.id}
+      </td>
+      <td className="px-2 py-1 text-center">{student.name}</td>
+      <ExamMarks
+        exams={props.exams}
+        getMark={props.getMark}
+        studentId={student.id}
+      />
+    </tr>
+  ));
+};
 
 const Result = (props) => {
   const [hasData, setHasData] = useState(false);
@@ -22,39 +61,9 @@ const Result = (props) => {
         examModelId === eid && studentModelId === sid
     );
 
+    if (row === undefined) return null;
+
     return row.mark;
-  };
-
-  const renderExamMarks = (studentId) => {
-    return exams.data.map((exam, index) => {
-      const mark = getMark(studentId, exam.id);
-      return (
-        <td key={index} className="px-2 py-1 text-center">
-          {mark === null ? "No attendance" : mark}
-        </td>
-      );
-    });
-  };
-
-  const renderResults = () => {
-    return students.data.map((student, index) => (
-      <tr key={index} className="border-b-2 border-black border-opacity-20">
-        <td className="px-2 py-1 text-center grid justify-items-center">
-          {student.id}
-        </td>
-        <td className="px-2 py-1 text-center">{student.name}</td>
-        {renderExamMarks(student.id)}
-      </tr>
-    ));
-  };
-
-  const renderExams = () => {
-    if (exams.data.length != 0) setHasData(true);
-    return exams.data.map((exam, index) => (
-      <th key={index} className="sticky top-0 py-2 bg-blue-200">
-        {exam.title}
-      </th>
-    ));
   };
 
   return (
@@ -65,30 +74,41 @@ const Result = (props) => {
         </div>
       </div> */}
 
-      {exams.isLoading || students.isLoading || studentTakesExam.isLoading ? (
-        <Loading />
-      ) : (
-        <table className="w-full table-auto">
-          <thead>
-            <tr className="text-rmit-blue">
-              <th className="sticky top-0 py-2 bg-blue-200">ID</th>
-              <th className="sticky top-0 py-2 bg-blue-200">Name</th>
-              {renderExams()}
-            </tr>
-          </thead>
-          <tbody>
-            {hasData ? (
-              renderResults()
-            ) : (
-              <tr>
-                <td colSpan="2" className="text-center">
-                  No results recorded
-                </td>
-              </tr>
-            )}
-          </tbody>
-        </table>
-      )}
+      {exams.isLoading ||
+        students.isLoading ||
+        (studentTakesExam.isLoading ? (
+          <Loading />
+        ) : (
+          <>
+            <table className="w-full table-auto">
+              <thead>
+                <tr className="text-rmit-blue">
+                  <th className="sticky top-0 py-2 bg-blue-200">ID</th>
+                  <th className="sticky top-0 py-2 bg-blue-200">Name</th>
+                  <Exams exams={exams} setHasData={setHasData} />
+                </tr>
+              </thead>
+              <tbody>
+                {hasData ? (
+                  <Results
+                    students={students}
+                    exams={exams}
+                    getMark={getMark}
+                  />
+                ) : (
+                  <tr>
+                    <td colSpan="2" className="text-center">
+                      No results recorded
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+            <div className="pt-5 text-xs text-gray-400">
+              * DNA: Did not attend
+            </div>
+          </>
+        ))}
     </>
   );
 };
