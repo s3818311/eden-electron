3d8ab47b0de9905b7938f830016489dbc0948593
tuyenhn <hntuyen282@gmail.com>
2021-05-27 17:48:24 +0700

    added exam status

--- src/examiner/processes/sequelize/models/exam.model.js
+++ src/examiner/processes/sequelize/models/exam.model.js
@@ -11,7 +11,7 @@ module.exports = (sequelize) => {
     status: {
       allowNull: false,
       type: DataTypes.STRING,
-      isIn: [["NONE", "LAUNCHED"]],
+      isIn: [["NONE", "LAUNCHED", "IN_SESSION"]],
     },
     title: {
       allowNull: false,
