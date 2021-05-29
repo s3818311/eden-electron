7febc5df8e8946437bfb40baf69a119fb0b01da0
s3822042 <s3822042@rmit.edu.vn>
2021-05-30 03:10:14 +0700

    add show score page component

---
+++ src/examinee/components/pages/ShowScore/ShowScorePage.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/prop-types */
+import React, { useEffect } from "react";
+import "./ShowScore.css";
+
+
+const ShowScorePage = (props) => {
+  const score = props.score;
+
+  useEffect(() => {
+    const ratings = document.querySelectorAll(".rating");
+    ratings.forEach((rating) => {
+      const ratingContent = score;
+      const ratingScore = parseInt(score, 10);
+
+      const scoreClass =
+        ratingScore < 40 ? "bad" : ratingScore < 60 ? "average" : "good";
+
+      rating.classList.add(scoreClass);
+
+      const ratingColor = window.getComputedStyle(rating).backgroundColor;
+
+      const gradient = `background: conic-gradient(${ratingColor} ${ratingScore}%, transparent 0 100%)`;
+
+      rating.setAttribute("style", gradient);
+
+      rating.innerHTML = `<span>${score} ${
+        ratingContent.indexOf("%") >= 0 ? "<small>%</small>" : ""
+      }</span>`;
+    });
+    return () => {};
+  }, []);
+  return (
+    <div className="flex flex-col items-center justify-center w-full h-screen">
+      <div className="mx-auto mt-20 text-6xl">Score</div>
+      <div className="rating">{props.score}</div>
+      <div>{props.score} Out of 100</div>
+    </div>
+  );
+};
+
+export default ShowScorePage;
---
+++ src/examinee/views/ShowScoreView.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+import ShowScorePage from "../components/pages/ShowScore/ShowScorePage";
+
+const ShowScoreView = () => <ShowScorePage />;
+
+export default ShowScoreView;
